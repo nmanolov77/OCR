b4ef5718815241291543776dc7668277cdea3725
Nick Manolov <nmanolov77@gmail.com>
2019-06-28 12:45:49 -0400

    initial commit

---
+++ cam/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace cam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtOCROutput = new System.Windows.Forms.TextBox();
+            this.infoBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.snapshotBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snapshotBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Crimson;
+            this.button4.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(714, 221);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 83);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "CAPTURE";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // txtOCROutput
+            // 
+            this.txtOCROutput.BackColor = System.Drawing.SystemColors.Info;
+            this.txtOCROutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 31.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOCROutput.Location = new System.Drawing.Point(846, 12);
+            this.txtOCROutput.Multiline = true;
+            this.txtOCROutput.Name = "txtOCROutput";
+            this.txtOCROutput.Size = new System.Drawing.Size(680, 292);
+            this.txtOCROutput.TabIndex = 10;
+            this.txtOCROutput.TextChanged += new System.EventHandler(this.TxtOCROutput_TextChanged);
+            // 
+            // infoBox
+            // 
+            this.infoBox.BackColor = System.Drawing.SystemColors.Info;
+            this.infoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoBox.Location = new System.Drawing.Point(846, 310);
+            this.infoBox.Multiline = true;
+            this.infoBox.Name = "infoBox";
+            this.infoBox.Size = new System.Drawing.Size(680, 282);
+            this.infoBox.TabIndex = 11;
+            this.infoBox.TextChanged += new System.EventHandler(this.InfoBox_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.ContextMenuStrip1_Opening);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::cam.Properties.Resources.ups;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(694, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(152, 200);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // snapshotBox
+            // 
+            this.snapshotBox.BackColor = System.Drawing.Color.Beige;
+            this.snapshotBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.snapshotBox.Location = new System.Drawing.Point(15, 310);
+            this.snapshotBox.Name = "snapshotBox";
+            this.snapshotBox.Size = new System.Drawing.Size(680, 282);
+            this.snapshotBox.TabIndex = 8;
+            this.snapshotBox.TabStop = false;
+            this.snapshotBox.Click += new System.EventHandler(this.SnapshotBox_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Beige;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(680, 292);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(1538, 607);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.infoBox);
+            this.Controls.Add(this.txtOCROutput);
+            this.Controls.Add(this.snapshotBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(385, 0);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Part Recognition";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snapshotBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox snapshotBox;
+        private System.Windows.Forms.TextBox txtOCROutput;
+        private System.Windows.Forms.TextBox infoBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ cam/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Tesseract;
+
+namespace cam
+{
+    public partial class Form1 : Form
+    {
+        VideoCaptureDevice frame;
+        FilterInfoCollection Devices;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Start_Cam() {
+            Devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            frame = new VideoCaptureDevice(Devices[1].MonikerString);
+            frame.NewFrame += new AForge.Video.NewFrameEventHandler(NewFrame_event);
+            frame.Start();
+
+        }
+        
+        void NewFrame_event(object send, NewFrameEventArgs e) {
+            try
+            {
+                pictureBox1.Image = (Image)e.Frame.Clone();
+            }
+
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //START BUTTON
+            Start_Cam();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //STOP BUTTON
+            frame.Stop();
+            pictureBox1.Image = null;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //BROWSE BUTTON
+           
+        }
+
+        string formatString(string input)
+        {
+
+            string formattedString = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input, i))
+                {
+                    formattedString += input[i];
+                }   
+            }
+            return formattedString;
+        }
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            //CAPTURE BUTTON
+            Random rnd = new Random();
+            int num = rnd.Next(1, 100000);
+            String output = @"C:\Users\imano\OneDrive\Desktop\New Folder\image" + num + ".png";
+            infoBox.Clear();
+            if (output != "" && pictureBox1.Image != null) {
+
+                pictureBox1.Image.Save(output);
+                // pictureBox1.Image.Save(output + "\\Image.png");
+                snapshotBox.Image = Image.FromFile(output);
+                var img = new Bitmap(Image.FromFile(output));
+                var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+                var page = ocr.Process(img);
+                string tessOut = page.GetText();
+                string gems = formatString(tessOut);
+
+                txtOCROutput.Text = "\n GEMS_ID: " + gems;
+
+                get_info(gems);
+            }
+        }
+        void get_info(String id)
+        {
+            String name;
+            String college;
+            String manager;
+            String role;
+            
+            
+
+            if (id == "5951043")
+            {
+                name = "Jacob Storms\n";
+                college = "University of Kentucky \n";
+                manager = "Tom Cecil \n";
+                role = "App Dev Intern \n";
+                infoBox.AppendText("Name: " +name);
+                infoBox.AppendText("College: " + college);
+                infoBox.AppendText("Manager: " +manager);
+                infoBox.AppendText("Role: " +role);
+
+            }
+            else if (id == "4158767")
+            {
+                //Chris
+                name = "Chris Ellis\n";
+                college = "Indiana University Southeast \n";
+                manager = "Tom Cecil \n";
+                role = "App Dev Co-op \n";
+                infoBox.AppendText("Name: " + name);
+                infoBox.AppendText("College: " + college);
+                infoBox.AppendText("Manager: " + manager);
+                infoBox.AppendText("Role: " + role);
+            }
+            else if (id == "5146103")
+            {
+                //Jeff
+                name = "Jeff Davis\n";
+                college = "University of Louisville \n";
+                manager = "Tom Cecil \n";
+                role = "Quality Assurance \n";
+                infoBox.AppendText("Name: " + name);
+                infoBox.AppendText("College: " + college);
+                infoBox.AppendText("Manager: " + manager);
+                infoBox.AppendText("Role: " + role);
+            }
+            else if (id == "3561590")
+            {
+                //Oumar
+                name = "Oumar Sall\n";
+                college = "University of Louisville \n";
+                manager = "Steve Weir \n";
+                role = "Application Developer \n";
+                infoBox.AppendText("Name: " + name);
+                infoBox.AppendText("College: " + college);
+                infoBox.AppendText("Manager: " + manager);
+                infoBox.AppendText("Role: " + role);
+            }
+            else
+            {
+                infoBox.AppendText("Invalid GemsID");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Start_Cam();
+        }
+
+        private void SnapshotBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtOCROutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InfoBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ContextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+    } 
+}
